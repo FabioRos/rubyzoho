a06524f607d55b087a2c68d682b5174e120a6732
FabioRos <fabio@moku.io>
2016-11-24 18:04:19 +0100

    bugfix

--- lib/crud_methods.rb
+++ lib/crud_methods.rb
@@ -64,6 +64,7 @@
 
       def mass_update(arr)
           arr.each_slice(100) do |slice|
+            logger.info slice
             # convert each to field values hash
             RubyZoho.configuration.api.bulk_update(self.module_name, slice)
           end
